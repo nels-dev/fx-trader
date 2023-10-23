c83600a27aad57a572a7530425675e218c7df609
nels-dev <101234025+nels-dev@users.noreply.github.com>
2023-10-23 13:11:34 -0700

    + health check endpoint

---
+++ backend/src/main/java/com/github/nelsdev/fxassist/common/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.github.nelsdev.fxassist.common.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+  public ResponseEntity<?> healthCheck(){
+    return ResponseEntity.ok().build();
+  }
+}
--- infrastructure/lib/application-stack.ts
+++ infrastructure/lib/application-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from "aws-cdk-lib";
+import {RemovalPolicy} from "aws-cdk-lib";
 import {Construct} from 'constructs';
-import * as ecr from "aws-cdk-lib/aws-ecr"
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as ecs from 'aws-cdk-lib/aws-ecs'
 import {LogDriver} from 'aws-cdk-lib/aws-ecs'
+import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import {UpdatePolicy} from "aws-cdk-lib/aws-autoscaling";
 import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
 
@@ -11,10 +12,7 @@ export class ApplicationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // Public repo allows more storage in the free tier
-    const ecrPublicRepository = ecr.Repository.fromRepositoryName(this, "repository", "public.ecr.aws/h7u1f7w9/fx-assist-backend-repository")
-
-    const cluster = new ecs.Cluster(this, 'FxAssistBackendCluster', {});
+    const cluster = new ecs.Cluster(this, 'FxAssistBackendCluster', {clusterName: 'FxAssistBackendCluster'});
 
     // Add EC2 instances as capacity to the cluster. Managed by an auto scaling group which only allow at most 1 active instance.
     cluster.addCapacity('DefaultAutoScalingGroup', {
@@ -30,16 +28,52 @@ export class ApplicationStack extends cdk.Stack {
       })
     })
 
+    // Setup load balancer and target group
+    const securityGroup = new ec2.SecurityGroup(this, 'applicationSecurityGroup', {
+      vpc: cluster.vpc,
+      allowAllOutbound: true
+    })
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80))
+
+    const alb = new elb.ApplicationLoadBalancer(this, 'loadBalancer', {
+      vpc: cluster.vpc,
+      internetFacing: true,
+      securityGroup: securityGroup,
+    })
+
+    const targetGroup = alb.addListener('listener', {port: 80, open: true}).addTargets('ecsTarget', {
+      port: 80
+    })
+
 
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'FxAssistBackendTask', {
       family:'ApplicationStackFxAssistBackendTask',
     });
-    const container = taskDefinition.addContainer('Application', {
+
+    // A placeholder container
+    taskDefinition.addContainer('Application', {
       containerName: 'Application',
-      image: ecs.ContainerImage.fromRegistry('alpine'),
+      image: ecs.ContainerImage.fromRegistry('public.ecr.aws/h7u1f7w9/fx-assist-backend-repository:6ba277f869940913bc41acf27df24884af6f0792'),
       memoryLimitMiB: 512,
-      command: ['tail', '-f', '/dev/null']
+      portMappings: [
+        {
+          hostPort: 80,
+          containerPort: 8080
+        }
+      ],
+      environment:{
+        "MONGODB_PASSWORD": process.env.MONGO_PASSWORD || ''
+      },
+      logging: LogDriver.awsLogs({streamPrefix: 'backend', logGroup: new LogGroup(this, 'BackendLogGroup', {logGroupName: '/application/FxAssistBackend', retention: RetentionDays.FIVE_DAYS, removalPolicy: RemovalPolicy.DESTROY})})
     })
-    new ecs.Ec2Service(this, 'Service', {cluster, taskDefinition, desiredCount: 1, minHealthyPercent: 0, maxHealthyPercent: 100})
+    const service = new ecs.Ec2Service(this, 'Service', {
+      serviceName: 'FxAssistBackendService',
+      cluster,
+      taskDefinition,
+      minHealthyPercent: 0,
+      maxHealthyPercent: 100,
+    })
+
+    targetGroup.addTarget(service)
   }
 }
