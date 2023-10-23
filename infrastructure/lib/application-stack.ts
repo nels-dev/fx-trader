6ba277f869940913bc41acf27df24884af6f0792
nels-dev <101234025+nels-dev@users.noreply.github.com>
2023-10-23 11:20:15 -0700

    + deploy backend application

--- infrastructure/lib/application-stack.ts
+++ infrastructure/lib/application-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import * as ecr from "aws-cdk-lib/aws-ecr"
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as ecs from 'aws-cdk-lib/aws-ecs'
+import {LogDriver} from 'aws-cdk-lib/aws-ecs'
 import {UpdatePolicy} from "aws-cdk-lib/aws-autoscaling";
+import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
 
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,6 +15,8 @@ export class ApplicationStack extends cdk.Stack {
     const ecrPublicRepository = ecr.Repository.fromRepositoryName(this, "repository", "public.ecr.aws/h7u1f7w9/fx-assist-backend-repository")
 
     const cluster = new ecs.Cluster(this, 'FxAssistBackendCluster', {});
+
+    // Add EC2 instances as capacity to the cluster. Managed by an auto scaling group which only allow at most 1 active instance.
     cluster.addCapacity('DefaultAutoScalingGroup', {
       instanceType: new ec2.InstanceType('t2.micro'),
       desiredCapacity: 1,
@@ -26,17 +30,16 @@ export class ApplicationStack extends cdk.Stack {
       })
     })
 
+
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'FxAssistBackendTask', {
-
+      family:'ApplicationStackFxAssistBackendTask',
     });
-    const container = taskDefinition.addContainer('Application',{
-      image: ecs.ContainerImage.fromRegistry(ecrPublicRepository.repositoryName||'None', {}),
-      portMappings: [
-        {containerPort: 8080, hostPort: 80}
-      ],
-      memoryLimitMiB: 512
+    const container = taskDefinition.addContainer('Application', {
+      containerName: 'Application',
+      image: ecs.ContainerImage.fromRegistry('alpine'),
+      memoryLimitMiB: 512,
+      command: ['tail', '-f', '/dev/null']
     })
-
-    new ecs.Ec2Service(this, 'Service', {cluster, taskDefinition})
+    new ecs.Ec2Service(this, 'Service', {cluster, taskDefinition, desiredCount: 1, minHealthyPercent: 0, maxHealthyPercent: 100})
   }
 }
